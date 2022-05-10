1ff7592256f4303dad21abba446f7f13aa57c31c
AlexSelMarburg <Selalx2@aol.com>
2022-05-11 01:36:35 +0200

    model now getWeightData calculation of avg. kcals fixed

--- js/model.js
+++ js/model.js
@@ -7,12 +7,29 @@ export const state = {
   kcalsAccumulator: 0,
   weightValue: '',
   data: [
-    // ['2022-04-16', 110],
-    // ['2022-04-16', 110],
-    // ['2022-04-16', 110],
+    // ['2022-04-16', 1805kcals, 102,3kg],
+    // ['2022-04-16', 1805kcals, 102,3kg],
+    // ['2022-04-16', 1805kcals, 102,3kg],
   ],
   DNS: {},
 };
+
+// const stateData = [
+//   ['2022-04-03', 2000, 102.5],
+//   ['2022-04-04', 2000],
+//   ['2022-04-05', 1000],
+//   ['2022-04-06', 2000, 100.5],
+//   ['2022-04-07', 1000],
+//   ['2022-04-08', 1000],
+//   ['2022-04-09', 2000],
+//   ['2022-04-10', 1000],
+//   ['2022-04-11', 1500, 101.5],
+//   ['2022-04-12', 2000],
+//   ['2022-04-13', 1000],
+//   ['2022-04-15', 2000],
+//   ['2022-04-16', 1000],
+//   ['2022-04-14', 2000, 102.3],
+// ];
 
 // #region INITIALIZATION AND DATA PERSISTENCE
 const init = function () {
@@ -142,24 +159,87 @@ export const getWeightData = function (weightValue = '') {
 
   // TODO: pagination beachten
 
-  // TODO: kcals durchschnitt zw. den berechnenen vergessen!!!
-  // 1) arrays mit länge 3 (reversed) aus state.data
-  const measurements = [];
-  for (let i = state.data.length - 1; i >= 0; i--) {
-    if (state.data[i].length >= 3) measurements.push(state.data[i]);
-  }
+  // #################################################
+  // ['2022-04-16', 1805kcals, 102,3kg]
 
-  // 2) push differece to arr
-  for (let i = 0; i < measurements.length; i++) {
-    if (i !== measurements.length - 1) {
-      measurements[i][3] = (
-        measurements[i][2] - measurements[i + 1][2]
-      ).toFixed(1);
-    } else {
-      measurements[i][3] = 'k.A';
+  let foundStart = false;
+  let foundNext = false;
+  let nextIsNeverFound = true;
+
+  let currKcalsAcc = 0;
+  let foundKcalsCount = 0;
+
+  let lastWeightValue = 0;
+
+  let currentMeasurment = [];
+  const measurements = [];
+
+  //TODO: state data
+  for (let i = state.data.length - 1; i >= 0; i--) {
+    if (foundStart) {
+      currKcalsAcc += state.data[i][1];
+      foundKcalsCount++;
+    }
+
+    if (state.data[i].length >= 3 && foundStart) {
+      foundNext = true;
+      nextIsNeverFound = false;
+    }
+
+    if (foundNext) {
+      currentMeasurment[1] = Math.round(currKcalsAcc / foundKcalsCount);
+      currentMeasurment[3] = (lastWeightValue - state.data[i][2]).toFixed(1);
+      measurements.push(currentMeasurment);
+      currKcalsAcc = foundKcalsCount = 0;
+      lastWeightValue = state.data[i][2];
+      foundNext = false;
+      currentMeasurment = [
+        state.data[i][0], //date
+        undefined, // avg-kcals
+        state.data[i][2], //weight
+        undefined, // weight-diference
+      ];
+    }
+
+    if (state.data[i].length >= 3 && !foundStart) {
+      currentMeasurment = [
+        state.data[i][0], //date
+        undefined, // avg-kcals
+        state.data[i][2], //weight
+        undefined, // weight-diference
+      ];
+
+      lastWeightValue = state.data[i][2];
+      foundStart = true;
     }
   }
+
+  if (foundStart && (nextIsNeverFound || currentMeasurment[1] === undefined)) {
+    currentMeasurment[1] = currentMeasurment[3] = 'k.A';
+    measurements.push(currentMeasurment);
+  }
+
+  // #################################################
+
+  // 1) arrays mit länge 3 (reversed) aus state.data
+  // const measurements = [];
+  // for (let i = state.data.length - 1; i >= 0; i--) {
+  //   if (state.data[i].length >= 3) measurements.push(state.data[i]);
+  // }
+
+  // // 2) push differece to arr
+  // for (let i = 0; i < measurements.length; i++) {
+  //   if (i !== measurements.length - 1) {
+  //     measurements[i][3] = (
+  //       measurements[i][2] - measurements[i + 1][2]
+  //     ).toFixed(1);
+  //   } else {
+  //     measurements[i][3] = 'k.A';
+  //   }
+  // }
+
   data.measurements = measurements;
+  console.log(data.measurements);
 
   return data;
 };
